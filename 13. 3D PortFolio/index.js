d6b5dbb7f1b8d98e79072f5222ba1fca32a36b66
Siddhant <siddhant.sharma1974@gmail.com>
2021-06-10 20:10:27 +0545

    New one added

--- 13. 3D PortFolio/index.js
+++ 13. 3D PortFolio/index.js
@@ -1,4 +1,4 @@
-import * as THREE from '/node_modules/three/build/three.module.js' //importing three.js as THREE
+import * as THREE from './node_modules/three/build/three.module.js' //importing three.js as THREE
 
 var xc=window.innerWidth/2;
 var yc=window.innerWidth/2;
