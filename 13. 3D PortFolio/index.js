7c7b08187fabe48ca24a6613da6b802cf61afa5c
Siddhant <siddhant.sharma1974@gmail.com>
2021-06-10 21:23:21 +0545

    New one added

--- 13. 3D PortFolio/index.js
+++ 13. 3D PortFolio/index.js
@@ -4,6 +4,7 @@ var xc=window.innerWidth/2;
 var yc=window.innerWidth/2;
 var scene = new THREE.Scene();
 var stop_flag=0;
+var hover_flag=0;
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
 camera.position.z = 5;
 
@@ -83,7 +84,7 @@ scene.add( circle );
 //microphone for communication.
 const geometry2 = new THREE.CircleGeometry( 0.25, 32 );
 const edges2 = new THREE.EdgesGeometry( geometry2 );
-const material4= new THREE.LineBasicMaterial( { color: 0xffffff} )
+const material4= new THREE.LineDashedMaterial( { color: 0xffffff} )
 const line1 = new THREE.LineSegments( edges2,material4)
 line1.position.set(3.74,2)
 scene.add( line1 );
@@ -101,17 +102,26 @@ var render=function(){
 
   renderer.render(scene,camera);
   (stop_flag)
+  if (hover_flag==1)
+  {
+    circle2.material=new THREE.MeshLambertMaterial( {map: texturemic} );
+  }
+  else {
+    circle2.material=new THREE.MeshLambertMaterial( {map: texturemichover} );
+  }
   if (stop_flag==0)
   {
     cube.rotation.y+=0.007;
     circle.rotation.z+=0.008;
     line1.rotation.z+=0.008;
-    //cube.rotation.x-=0.005;
   }
+
 }
+//calling render function
 
 var clicked_flag=0;
 var i=0;
+
 
 window.addEventListener("mousedown", event=>{
   clicked_flag=1;
@@ -126,17 +136,20 @@ window.addEventListener('mousemove',event=>{
   let hoverx=event.clientX-100;
   let hovery=event.clientY-110;
   console.log(hoverx,hovery);
-  if (hovery>25 && hovery<1050 && hoverx>150 && hoverx<1704 )
+  if (hovery>25 && hovery<1050 && hoverx>150 && hoverx<=2000 )
   {
     stop_flag=1;
-    console.log(hoverx,hovery)
-    console.log("stop o")
-    if(hoverx>1616 && hoverx<1704 && hovery>160 && hovery<240)
+
+    if(hoverx>=1800 && hoverx<=1950 && hovery>120 && hovery<=120+150 )
      {
        console.log('over the mic')
+       hover_flag=1;
      }
+     else {
+       hover_flag=0;
+     }
+     //
   }
-
   else {
     stop_flag=0;
   }
@@ -157,9 +170,7 @@ window.addEventListener('mousemove',event=>{
     }
   }
 })
-
-//adding voice feature;
-/*
+/*//adding voice feature;
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition =new SpeechRecognition();
 recognition.interimResults=true;
@@ -169,6 +180,7 @@ let p=document.createElement('p');
 recognition.addEventListener('result',e=>{
   console.log(e);
 })
-recognition.start();*/
-//calling render function
+recognition.start();
+//voice feature till here
+*/
 render();
